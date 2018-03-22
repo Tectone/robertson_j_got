62daa82658e62ac31a15424baacbd8df5e0856c1
John Robertson <tectone18@gmail.com>
2018-03-22 10:37:41 -0400

    dev.jrr.gotFinishedPart1
    everythings clean and ready to go for next class! Good job! :D

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
  //JavaScript Document
-
+(() => {
  console.log("Linked up");
+
+ // variable stack goes here
+ let sigils = document.querySelectorAll('.sigilContainer'),
+ 	lightbox = document.querySelector('.lightbox'),
+ 	closeLightBoxButton = lightbox.querySelector('.close-lightbox');
+
+ 	// functions in the middle!
+ 	function showHouseVideo() {
+ 		//debugger;
+ 		lightbox.classList.add('show-lightbox');
+ 		// make the video play
+ 	}
+
+ 	function closeLightbox() {
+ 		lightbox.classList.remove('show-lightbox');
+ 		// stop the video and rewind it to 0
+ 	}
+
+ 	//event handling at the bottomNa
+ 	//
+ 	sigils.forEach(sigil => sigil.addEventListener('click', showHouseVideo));
+ 	closeLightBoxButton.addEventListener('click', closeLightbox);
+})();

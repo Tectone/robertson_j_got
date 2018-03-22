dc805bc67a990f3811633be8e4204c5b119757f6
John Robertson <tectone18@gmail.com>
2018-03-22 09:48:27 -0400

    dev.jrr.finishedButtonDemo
    finished the play controls for all web browsers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
  //JavaScript Document
-
+(() => {
  console.log("Linked up");
+ // variables come first!
+ //grab the video
+ const vidPlayer = document.querySelector('video'),
+ 	pauseButton =document.querySelectorAll('button')[0],
+ 	playButton =document.querySelectorAll('button')[1],
+ 	rewindButton =document.querySelectorAll('button')[2];
+
+ 	//function go in the middle!
+ 	function volOn() {
+ 		vidPlayer.muted = false;
+ 	}
+
+ 	function volOff() {
+ 		vidPlayer.muted = true;
+ 	}
+
+ 	function playVideo() {
+ 		// make the video play!
+ 		vidPlayer.play();
+ 	}
+
+ 	function pauseVideo() {
+ 		// make the video pause!
+ 		vidPlayer.pause();
+ 	}
+
+ 	function rewindVideo() {
+ 		// make the video rewind!
+ 		//vidPlayer.currentTime -= 3;
+ 		vidPlayer.currentTime = 0;
+ 	}
+
+ 	vidPlayer.addEventListener('mouseover', volOn);
+ 	vidPlayer.addEventListener('mouseout', volOff);
+
+ 	playButton.addEventListener('click', playVideo);
+ 	pauseButton.addEventListener('click', pauseVideo);
+ 	rewindButton.addEventListener('click', rewindVideo);
+
+
+ })();
+
